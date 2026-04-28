cfc867de1d1258ea1a2e50907997b49d5e2ea8ca
Anubhavkumar31 <anubhav.kumar1@icloud.com>
2026-04-28 12:22:10 +0530

    deleted extra stuffs and refactor the modules in clean directories

--- backend/resources/old_codes/main_copy_manvi.py
+++ backend/resources/old_codes/main_copy_manvi.py
@@ -4482,7 +4482,7 @@ class MyMainWindow(QMainWindow):
     def open_PipeScheme(self):
         try:
             import subprocess, sys, os
-            pipeline_path = os.path.join("pipeline_schema", "pipeline_schema.py")
+            pipeline_path = os.path.join("main_window/components/menubar/report_menu/pipeline_schema", "pipeline_schema.py")
             subprocess.Popen([sys.executable, pipeline_path, self.project_root])
         except Exception as e:
             self.open_Error(f"Error running Pipeline Schema:\n{e}")
